5010ae93ad41d4ec3580b951347eaddfcc0ae9ab
youngsoon12 <enejwk789@gmail.com>
2023-02-21 18:01:31 +0900

    react-redux study...

---
+++ REDUX-VANILLA/react_redux/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Counter} from './features/counter/Counter';
+import './App.css';
+
+function App() {
+  console.log(Counter.counter);
+  return <div className='App'></div>;
+}
+
+export default App;
